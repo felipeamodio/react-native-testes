1b7a1e098752f2b2f02f4c7d0cedb82e878562c9
Felipe Alves <felipeaamodio@gmail.com>
2023-01-19 20:07:40 -0300

    checking api calls

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -31,17 +31,27 @@ const mockRequisicaoErro = () => {
 }
 
 describe('repositorio/leilao', () => {
+    beforeEach(() => { //função que vai rodar sempre antes de cada teste
+        apiLeiloes.get.mockClear(); //vai limpar a requisição. OBS: não vai limpar a implementação
+    });
+
     describe('obtemLeiloes', () => {
         test('deve retornar uma lista de leilões', async () => {
             apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
             const leiloes = await obtemLeiloes();
             expect(leiloes).toEqual(mockLeiloes) //pra objetos utiliza o toEqual()
+
+            expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes') //quer saber se o método foi chamado
+            expect(apiLeiloes.get).toHaveBeenCalledTimes(1) //quer saber quantas vezes foi chamado
         });
 
         test('deve retornar uma lista vazia quando a requisição falhar', async () => {
             apiLeiloes.get.mockImplementation(() => mockRequisicaoErro())
             const leiloes = await obtemLeiloes();
             expect(leiloes).toEqual([]) //pra objetos utiliza o toEqual()
+
+            expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes')
+            expect(apiLeiloes.get).toHaveBeenCalledTimes(1)
         });
     })
 })

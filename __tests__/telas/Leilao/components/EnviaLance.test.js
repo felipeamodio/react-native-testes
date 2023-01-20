42a127f7b0bc4eb89d3f55570b1359a7660c29ca
Felipe Alves <felipeaamodio@gmail.com>
2023-01-20 17:42:37 -0300

    manipulating component

--- __tests__/telas/Leilao/components/EnviaLance.test.js
+++ __tests__/telas/Leilao/components/EnviaLance.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import EnviaLance from '../../../../src/telas/Leilao/componentes/EnviaLance';
-import {ENVIADO} from '../../../../src/negocio/constantes/estadosLance';
+import {ENVIADO, NAO_ENVIADO} from '../../../../src/negocio/constantes/estadosLance';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 describe('telas/Leilao/components/Envialance', () => {
-    test('deve enviar o lance quando botão for pressionado', () => {
+    test('deve enviar o lance quando botão for pressionado', async () => {
         const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO))) //chamando uma função mockada
-        const {toJSON} = render(
+        const {getByPlaceholderText, getByA11yHint, getByText} = render(
             <EnviaLance 
                 enviaLance={enviaLance}
                 cor="blue"
             />
         );
-        console.log(toJSON())
+
+        const input = getByPlaceholderText("R$");
+        const botao = getByA11yHint("Enviar lance");
+
+        fireEvent.changeText(input, '10');
+        fireEvent.press(botao);
+
+        expect(enviaLance).toHaveBeenCalledWith('10');
+        await waitFor(() => {
+            expect(getByText(ENVIADO)).toBeTruthy();
+        });
+        expect(() => getByText(NAO_ENVIADO)).toThrow();
     })
 })

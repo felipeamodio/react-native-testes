9873f4e56d84f5d75b79f6d4b68e022a385946b5
Felipe Alves <felipeaamodio@gmail.com>
2023-01-20 16:39:50 -0300

    updated tests

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -1,57 +1,79 @@
-import {obtemLeiloes} from '../../src/repositorio/leilao';
+import { obtemLeiloes, obtemLeilao } from '../../src/repositorio/leilao';
 import apiLeiloes from '../../src/servicos/apiLeiloes';
 
-//mockando o arquivo pra testes
 jest.mock('../../src/servicos/apiLeiloes');
 
 const mockLeiloes = [
-    {
-        id: 1,
-        nome: 'Leilão',
-        descricao: 'Descrição do leilão'
-    }
+  {
+    id: 1,
+    nome: 'Leilão',
+    descricao: 'Descrição do leilão'
+  }
 ];
 
 const mockRequisicao = (retorno) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                data: retorno
-            })
-        }, 200)
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      })
+    }, 200);
+  });
 }
 
 const mockRequisicaoErro = () => {
-    return new Promise((_, reject) => {
-        setTimeout(() => {
-            reject()
-        }, 200)
-    });
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
 }
 
-describe('repositorio/leilao', () => {
-    beforeEach(() => { //função que vai rodar sempre antes de cada teste
-        apiLeiloes.get.mockClear(); //vai limpar a requisição. OBS: não vai limpar a implementação
+describe('respositorio/leilao', () => {
+
+  beforeEach(() => {
+    apiLeiloes.get.mockClear();
+  });
+
+  describe('obtemLeiloes', () => {
+
+    test('deve retornar uma lista de leilões', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
+      
+      const leiloes = await obtemLeiloes();
+
+      expect(leiloes).toEqual(mockLeiloes);
+
+      expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
     });
 
-    describe('obtemLeiloes', () => {
-        test('deve retornar uma lista de leilões', async () => {
-            apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
-            const leiloes = await obtemLeiloes();
-            expect(leiloes).toEqual(mockLeiloes) //pra objetos utiliza o toEqual()
+    test('deve retornar uma lista vazia quando a requisição falhar', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+      
+      const leiloes = await obtemLeiloes();
 
-            expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes') //quer saber se o método foi chamado
-            expect(apiLeiloes.get).toHaveBeenCalledTimes(1) //quer saber quantas vezes foi chamado
-        });
+      expect(leiloes).toEqual([]);
 
-        test('deve retornar uma lista vazia quando a requisição falhar', async () => {
-            apiLeiloes.get.mockImplementation(() => mockRequisicaoErro())
-            const leiloes = await obtemLeiloes();
-            expect(leiloes).toEqual([]) //pra objetos utiliza o toEqual()
+      expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
+    });
 
-            expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes')
-            expect(apiLeiloes.get).toHaveBeenCalledTimes(1)
-        });
-    })
-})
+  });
+
+  describe('obtemLeilao', () => {
+    test('deve retornar um leilão', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes[0]));
+
+      const leilao = await obtemLeilao(1);
+      expect(leilao).toEqual(mockLeiloes[0]);
+    });
+
+    test('deve retornar um objeto vazio caso erro na requisição', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+
+      const leilao = await obtemLeilao(1);
+      expect(leilao).toEqual({});
+    });
+  });
+});
